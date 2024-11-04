59cc512b11800d1fb7a1f25c9d61efb54aa2abfa
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-11-04 21:07:32 +0100

    Ok, .has di set è migliore di includes

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,57 @@
 // Video youtube da cui prendo spunto: https://www.youtube.com/watch?v=_pWA4rbzvIg
-// TODO: provalo anche con DENO !! Deno.bench per benchmark
 
 let ourArr: string[] = [];
-const ourSet: Set<string> = new Set<string>();  //TODO: const or let?
-// TODO: How to properly declaare and assign a set?
+const ourSet: Set<string> = new Set<string>();
 
-const yy = 1000 * 1000 * 10;
-for (let i = 0; i < yy; i++) {
+///// COSTRUZIONE ARRAY E SET ///////////
+// Costruzione array
+const inizioCostruzione = performance.now();
+const iterations = 1000 * 1000 * 10;
+for (let i = 0; i < iterations; i++) {
   const element = `ciaociao-${i}`;
   ourArr[i] = element;
+};
+const fineCostruzione = performance.now();
+const intervalloCostruzione = fineCostruzione - inizioCostruzione;
+console.log("Intervallo costruzione di array: ", intervalloCostruzione, "ms");
+
+// Costruzione set
+const inizioCostruzioneSet = performance.now();
+const iterationsSet = 1000 * 1000 * 10;
+for (let i = 0; i < iterationsSet; i++) {
+  const element = `ciaociao-${i}`;
   ourSet.add(element);
 };
+const fineCostruzioneSet = performance.now();
+const intervalloCostruzioneSet = fineCostruzioneSet - inizioCostruzioneSet;
+console.log("Intervallo costruzione del set: ", intervalloCostruzioneSet, "ms");
+///////////////////////////////////////////
 
-// TODO: quanto ci metto per costruire array e set?
-// TODO: setta tsconfig come altri progetti
+/** Stringa da cercare */
+const searchString = 'ciaociao-877566';
 
-// Performance includes
+// PERFORMANCE INCLUDES
 const beginIncludes = performance.now();
 
-const resIncludes = ourArr.includes('ciaociao-877566');
+const resIncludes = ourArr.includes(searchString);
+
 const finalIncludes = performance.now();
-console.log("resIncludes :", resIncludes);
+
+console.log("Elemento trovato con includes?", resIncludes);
 const intervalIncludes = finalIncludes - beginIncludes;
 
-// Performance set con .has
+// PERFORMANCE CON .HAS DI SET
 const beginHasSet = performance.now();
 
-const resHasSet = ourSet.has('ciaociao-877566');
+const resHasSet = ourSet.has(searchString);
+
 const finalHasSet = performance.now();
-console.log("resHasSet :", resHasSet);
-const intervalHasSet = finalIncludes - beginIncludes;
 
-// Logging
-console.log("Interval for includes:", intervalIncludes);
-console.log("Interval for .has di set", intervalHasSet);
+console.log("Elemento trovato con .has di set?", resHasSet);
+const intervalHasSet = finalHasSet - beginHasSet;
 
-// TODO: perché i res interval sono uguali??
+// Final logging
+console.log("##########################################");
+console.log("INTERVAL PERFORMANCE FOR INCLUDES:", intervalIncludes);
+console.log("INTERVAL PERFORMANCE FOR .HAS DI SET", intervalHasSet);
+console.log("RATIO BETWEEN THE TWO:", (intervalIncludes/intervalHasSet).toFixed(3));
