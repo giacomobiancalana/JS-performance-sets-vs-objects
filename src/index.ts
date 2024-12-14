29dcfc7dbcb98606f78b0a1c7a79c9a19e324d06
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-12-14 16:38:53 +0100

    npm update e roundFix function

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 // Video youtube da cui prendo spunto: https://www.youtube.com/watch?v=_pWA4rbzvIg
 
+///// COSTRUZIONE ARRAY E SET ///////////
 let ourArr: string[] = [];
 const ourSet: Set<string> = new Set<string>();
 
-///// COSTRUZIONE ARRAY E SET ///////////
-// Costruzione array
+///// POPOLAMENTO ARRAY E SET ///////////
+// Popolamento array
 const inizioCostruzione = performance.now();
-const iterations = 1000 * 1000 * 10;
+const iterations = 1000 * 1000 * 10;  // 10 milioni di iterazioni
 for (let i = 0; i < iterations; i++) {
   const element = `ciaociao-${i}`;
   ourArr[i] = element;
 };
 const fineCostruzione = performance.now();
-const intervalloCostruzione = fineCostruzione - inizioCostruzione;
+const intervalloCostruzione = roundFix(fineCostruzione - inizioCostruzione, 1);
 console.log("Intervallo costruzione di array: ", intervalloCostruzione, "ms");
 
-// Costruzione set
+// Popolamento set
 const inizioCostruzioneSet = performance.now();
 const iterationsSet = 1000 * 1000 * 10;
 for (let i = 0; i < iterationsSet; i++) {
@@ -23,7 +24,7 @@ for (let i = 0; i < iterationsSet; i++) {
   ourSet.add(element);
 };
 const fineCostruzioneSet = performance.now();
-const intervalloCostruzioneSet = fineCostruzioneSet - inizioCostruzioneSet;
+const intervalloCostruzioneSet = roundFix(fineCostruzioneSet - inizioCostruzioneSet, 1);
 console.log("Intervallo costruzione del set: ", intervalloCostruzioneSet, "ms");
 ///////////////////////////////////////////
 
@@ -38,7 +39,7 @@ const resIncludes = ourArr.includes(searchString);
 const finalIncludes = performance.now();
 
 console.log("Elemento trovato con includes?", resIncludes);
-const intervalIncludes = finalIncludes - beginIncludes;
+const intervalIncludes = roundFix(finalIncludes - beginIncludes, 1);
 
 // PERFORMANCE CON .HAS DI SET
 const beginHasSet = performance.now();
@@ -48,10 +49,14 @@ const resHasSet = ourSet.has(searchString);
 const finalHasSet = performance.now();
 
 console.log("Elemento trovato con .has di set?", resHasSet);
-const intervalHasSet = finalHasSet - beginHasSet;
+const intervalHasSet = roundFix(finalHasSet - beginHasSet, 1);
 
 // Final logging
 console.log("##########################################");
-console.log("INTERVAL PERFORMANCE FOR INCLUDES:", intervalIncludes);
-console.log("INTERVAL PERFORMANCE FOR .HAS DI SET", intervalHasSet);
-console.log("RATIO BETWEEN THE TWO:", (intervalIncludes/intervalHasSet).toFixed(3));
+console.log("INTERVAL PERFORMANCE FOR INCLUDES:", intervalIncludes, "ms");
+console.log("INTERVAL PERFORMANCE FOR .HAS DI SET", intervalHasSet, "ms");
+console.log("RATIO BETWEEN THE TWO:", roundFix(intervalIncludes/intervalHasSet, 3));
+
+function roundFix(num: number, precision: number = 0): number {
+  return parseFloat(num.toFixed(precision));
+}
