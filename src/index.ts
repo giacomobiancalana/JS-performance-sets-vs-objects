fe4d787ae8bb873519baebfeab79d186a5bc52d4
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2025-06-29 17:26:58 +0200

    Stringa con numero random, log del tempo migliorato

--- src/index.ts
+++ src/index.ts
@@ -9,54 +9,52 @@ const ourSet: Set<string> = new Set<string>();
 const inizioCostruzione = performance.now();
 const iterations = 1000 * 1000 * 10;  // 10 milioni di iterazioni
 for (let i = 0; i < iterations; i++) {
-  const element = `ciaociao-${i}`;
-  ourArr[i] = element;
+  ourArr[i] = `ciaociao-${i}`;
 };
 const fineCostruzione = performance.now();
-const intervalloCostruzione = roundFix(fineCostruzione - inizioCostruzione, 1);
+const intervalloCostruzione = roundFix(fineCostruzione - inizioCostruzione, 5);
 console.log("Intervallo costruzione di array: ", intervalloCostruzione, "ms");
 
 // Popolamento set
 const inizioCostruzioneSet = performance.now();
-const iterationsSet = 1000 * 1000 * 10;
-for (let i = 0; i < iterationsSet; i++) {
-  const element = `ciaociao-${i}`;
-  ourSet.add(element);
+for (let i = 0; i < iterations; i++) {
+  ourSet.add(`ciaociao-${i}`);
 };
 const fineCostruzioneSet = performance.now();
-const intervalloCostruzioneSet = roundFix(fineCostruzioneSet - inizioCostruzioneSet, 1);
+const intervalloCostruzioneSet = roundFix(fineCostruzioneSet - inizioCostruzioneSet, 5);
 console.log("Intervallo costruzione del set: ", intervalloCostruzioneSet, "ms");
 ///////////////////////////////////////////
 
+const numeroRandom = Math.trunc(Math.random() * iterations);  // tra 0 e iterationsSet - 1
 /** Stringa da cercare */
-const searchString = 'ciaociao-877566';
+const searchString = `ciaociao-${numeroRandom}`;
+console.log(`Stringa da cercare: ${searchString}`);
 
 // PERFORMANCE INCLUDES
 const beginIncludes = performance.now();
-
 const resIncludes = ourArr.includes(searchString);
-
 const finalIncludes = performance.now();
-
 console.log("Elemento trovato con includes?", resIncludes);
-const intervalIncludes = roundFix(finalIncludes - beginIncludes, 1);
+const intervalIncludes = finalIncludes - beginIncludes;
 
 // PERFORMANCE CON .HAS DI SET
 const beginHasSet = performance.now();
-
 const resHasSet = ourSet.has(searchString);
-
 const finalHasSet = performance.now();
-
 console.log("Elemento trovato con .has di set?", resHasSet);
-const intervalHasSet = roundFix(finalHasSet - beginHasSet, 1);
+const intervalHasSet = finalHasSet - beginHasSet;
 
 // Final logging
 console.log("##########################################");
-console.log("INTERVAL PERFORMANCE FOR INCLUDES:", intervalIncludes, "ms");
-console.log("INTERVAL PERFORMANCE FOR .HAS DI SET", intervalHasSet, "ms");
-console.log("RATIO BETWEEN THE TWO:", roundFix(intervalIncludes/intervalHasSet, 3));
+console.log("INTERVAL PERFORMANCE FOR INCLUDES:", roundFix(intervalIncludes, 5), "ms");
+console.log("INTERVAL PERFORMANCE FOR .HAS DI SET", roundFix(intervalHasSet, 5), "ms");
+console.log("RATIO BETWEEN THE TWO:", roundFix(intervalIncludes/intervalHasSet, 5));
 
+/**
+ * @param num numero da arrotondare
+ * @param precision numero di decimali ammissibili
+ * @returns numero arrotondato
+ */
 function roundFix(num: number, precision: number = 0): number {
   return parseFloat(num.toFixed(precision));
 }
